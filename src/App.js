39efad40953075bb3c3f74f4c58eed23e17dac85
Julia N <uxjulia@users.noreply.github.com>
2022-06-09 22:40:44 -0400

    Use blank resources

--- src/App.js
+++ src/App.js
@@ -131,37 +131,25 @@ function resetPlayers(oldTotal, newTotal, playerArr) {
   }
   return playerArr;
 }
-const testResources = {
-  2: { 1: "lumber" },
-  3: { 1: "brick", 2: "ore" },
-  4: { 1: "ore", 2: "lumber" },
-  5: { 1: "wool", 2: "brick" },
-  6: { 1: "grain", 2: "lumber" },
-  8: { 1: "ore", 2: "wool" },
-  9: { 1: "ore", 2: "brick" },
-  10: { 1: "lumber", 2: "brick" },
-  11: { 1: "ore", 2: "wool" },
-  12: { 1: "grain" },
-};
 
-// const blankResources =  {
-//     2: { 1: "" },
-//     3: { 1: "", 2: "" },
-//     4: { 1: "", 2: "" },
-//     5: { 1: "", 2: "" },
-//     6: { 1: "", 2: "" },
-//     8: { 1: "", 2: "" },
-//     9: { 1: "", 2: "" },
-//     10: { 1: "", 2: "" },
-//     11: { 1: "", 2: "" },
-//     12: { 1: "" },
-//   }
+ const blankResources =  {
+     2: { 1: "" },
+     3: { 1: "", 2: "" },
+     4: { 1: "", 2: "" },  
+     5: { 1: "", 2: "" },
+     6: { 1: "", 2: "" },
+     8: { 1: "", 2: "" },
+     9: { 1: "", 2: "" },
+     10: { 1: "", 2: "" },
+     11: { 1: "", 2: "" },
+     12: { 1: "" },
+   }
 
 const defaultState = {
   players: [],
   rolls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   log: [],
-  resources: testResources,
+  resources: blankResources,
   activePlayer: 0,
   fullScreen: false,
   diceRolls: [],
